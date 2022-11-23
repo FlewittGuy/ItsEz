867b9e3c5a4530b2ab7a72ad6752023f2a64d19f
Flewitt <98241516+FlewittGuy@users.noreply.github.com>
2022-11-22 21:04:53 -0500

    Added .idea to .gitignore

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ TOKEN = os.getenv('TOKEN')  # Get the TOKEN variable from the .env file
 guild_id = 847962669797474334
 
 
-class client(discord.Client):
+class Client(discord.Client):
     def __init__(self):
         super().__init__(intents=Intents.default())
         self.synced = False  # So the bot doesn't sync commands more than once
@@ -19,10 +19,12 @@ class client(discord.Client):
     async def on_ready(self):
         await self.wait_until_ready()
         if not self.synced:  # Check if commands have been synced
-            await tree.sync(guild = discord.Object(id=guild_id))  # Guild specific, leave blank if global (global registration can take up to a day)
+            await tree.sync(guild=discord.Object(id=guild_id))  # Guild specific, leave blank if global (global
+            # registration can take up to a day)
             self.synced = True
 
-        tree.clear_commands(guild = discord.Object(id=guild_id))  # Clear all commands from the previous command tree to avoid issues
+        tree.clear_commands(guild=discord.Object(id=guild_id))  # Clear all commands from the previous command tree
+        # to avoid issues
         print(f"Successfully cleared commands.")
         tree.add_command(command_list)
         tree.add_command(test_modal)
@@ -34,7 +36,7 @@ Initializing the modals that will be used later in the commands
 """
 
 
-class mrz_modal(ui.Modal, title = "Test Modal"):
+class MrzModal(ui.Modal, title="Test Modal"):
     answer = ui.TextInput(
         label="Is MrZ cool?",
         style=discord.TextStyle.short,
@@ -42,13 +44,13 @@ class mrz_modal(ui.Modal, title = "Test Modal"):
         max_length=3
     )
     confirmation = ui.TextInput(label="Are you sure?",
-        style=discord.TextStyle.short,
-        placeholder="Answer", required=True,
-        max_length=3
-    )
+                                style=discord.TextStyle.short,
+                                placeholder="Answer", required=True,
+                                max_length=3
+                                )
 
     async def on_submit(self, interaction: Interaction):
-        embed = discord.Embed(title=self.title, timestamp=datetime.now(), color=Color.from_rgb(47,49,54))
+        embed = discord.Embed(title=self.title, timestamp=datetime.now(), color=Color.from_rgb(47, 49, 54))
         embed.set_author(name=interaction.user, icon_url=interaction.user.avatar)
         embed.add_field(name=self.answer.label, value=self.answer, inline=False)
         embed.add_field(name=self.confirmation.label, value=self.confirmation, inline=False)
@@ -56,9 +58,8 @@ class mrz_modal(ui.Modal, title = "Test Modal"):
 
 
 # Initializing the client and command tree
-client = client()
+client = Client()
 tree = app_commands.CommandTree(client)
-
 
 """
 The follow commands are Test Commands
@@ -67,23 +68,24 @@ The follow commands are Test Commands
 
 # Fetch all the commands that exist in the bot's code
 @tree.command(
-    guild = discord.Object(id=guild_id), 
-    name = 'command-list', 
+    guild=discord.Object(id=guild_id),
+    name='command-list',
     description='This is a developer intended command, but feel free to try it out.',
 )
 async def command_list(interaction: discord.Interaction):
-    fetched = await tree.fetch_commands(guild = discord.Object(id=guild_id))  # Fetch all the commands registered with the command tree
-    await interaction.response.send_message(fetched, ephemeral = True)
+    fetched = await tree.fetch_commands(
+        guild=discord.Object(id=guild_id))  # Fetch all the commands registered with the command tree
+    await interaction.response.send_message(fetched, ephemeral=True)
 
 
 # Generate a modal that asks the user if MrZ is a cool person (they are)
 @tree.command(
-    guild = discord.Object(id=guild_id), 
-    name = 'test-modal', 
+    guild=discord.Object(id=guild_id),
+    name='test-modal',
     description='Discord modal test.',
 )
 async def test_modal(interaction: discord.Interaction):
-    await interaction.response.send_modal(mrz_modal())
+    await interaction.response.send_modal(MrzModal())
 
 
 client.run(TOKEN)
